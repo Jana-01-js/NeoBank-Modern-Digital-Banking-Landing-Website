0ee6302d22e0f0fe48c6161af25d12c80d1f9152
Janarthanan R <rkkj622@gmail.com>
2026-06-30 18:10:06 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -45,3 +45,70 @@ navbar.style.boxShadow="none";
 }
 
 });
+/* ======================
+Dark Mode
+====================== */
+
+const themeBtn = document.getElementById("theme-toggle");
+
+themeBtn.onclick = () => {
+
+document.body.classList.toggle("light-theme");
+
+themeBtn.innerHTML =
+document.body.classList.contains("light-theme")
+? "🌞"
+: "🌙";
+
+};
+
+/* ======================
+Mobile Menu
+====================== */
+
+const menu = document.querySelector(".menu-toggle");
+
+const navLinks = document.querySelector(".nav-links");
+
+menu.onclick = ()=>{
+
+navLinks.classList.toggle("active");
+
+};
+
+/* ======================
+Scroll Animation
+====================== */
+
+const revealElements=document.querySelectorAll(
+".card,.price-card,.testimonial-card,.partner-card,.stat"
+);
+
+window.addEventListener("scroll",()=>{
+
+const trigger=window.innerHeight*0.85;
+
+revealElements.forEach((el)=>{
+
+const top=el.getBoundingClientRect().top;
+
+if(top<trigger){
+
+el.style.opacity="1";
+
+el.style.transform="translateY(0)";
+
+}
+
+});
+
+});
+
+revealElements.forEach((el)=>{
+
+el.style.opacity="0";
+
+el.style.transform="translateY(40px)";
+el.style.transition=".7s";
+
+});
